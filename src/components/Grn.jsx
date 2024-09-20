6fdbe8fe5d2679e65655e51bc20aaaec7c82271e
kedar <patilkedar147@gmail.com>
2024-09-20 14:17:57 +0530

    adjusted rn page

--- src/components/Grn.jsx
+++ src/components/Grn.jsx
@@ -266,6 +266,15 @@ const Grn = () => {
               readOnly
             />
           </div>
+          <div>
+            <label htmlFor='batchNumber'>Vendor Invoice:</label>
+            <input
+              type='text'
+              id='batchNumber'
+              value={vendorInvoice}
+
+            />
+          </div>
 
           {purchaseOrderId && (
             <div>
