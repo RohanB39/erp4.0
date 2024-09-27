6acf0a87860bd21a3bd1b69c0d93f60d93f713b3
Rohan Bankar <96434805+RohanB39@users.noreply.github.com>
2024-09-27 17:10:52 +0530

    Vite restored

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            if (id.includes('react')) {
-              return 'react-vendors';
-            }
-            if (id.includes('html2canvas')) {
-              return 'html2canvas';
-            }
-            return 'vendor';
-          }
-        }
-      }
-    },
-    chunkSizeWarningLimit: 4000, 
-  },
-});
+})
