05f19a34058d25f1e5982aa8f4363ca8688421a3
Rohan Bankar <96434805+RohanB39@users.noreply.github.com>
2024-09-27 17:01:32 +0530

    Buid done

--- vite.config.js
+++ vite.config.js
@@ -6,23 +6,19 @@ export default defineConfig({
   build: {
     rollupOptions: {
       output: {
-        // Manual code splitting for libraries
         manualChunks(id) {
           if (id.includes('node_modules')) {
-            // Split React and its dependencies
             if (id.includes('react')) {
               return 'react-vendors';
             }
-            // Split html2canvas and similar larger libraries
             if (id.includes('html2canvas')) {
               return 'html2canvas';
             }
-            // All other third-party libraries
             return 'vendor';
           }
         }
       }
     },
-    chunkSizeWarningLimit: 1000,  // Set a chunk size warning limit (default is 500kB)
+    chunkSizeWarningLimit: 4000, 
   },
 });
