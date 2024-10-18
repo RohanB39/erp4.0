0dfee2fda6434c0816742444850f00bee45a4508
Rohan Bankar <96434805+RohanB39@users.noreply.github.com>
2024-10-18 15:08:15 +0530

    existong grn page

---
+++ src/components/store/grn/existingMaterialGrn/ExistingMaterialGrn.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './existingMaterialGrn.css';
+
+const ExistingMaterialGrn = () => {
+  return (
+    <div className='main' id='main'>
+        
+    </div>
+  )
+}
+
+export default ExistingMaterialGrn
